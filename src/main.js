aa3121c371ba260e777178a826c6faae042e4f8c
ka1sser <serenokaisser.dev@gmail.com>
2026-02-19 23:31:16 +0800

    Done fixing navigation for mobile

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
+const primaryHeader = document.querySelector(".primary-header");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".primary-navigation");
 
 navToggle.addEventListener("click", () => {
-    primaryNav.hasAttribute("data-visible")
-      ? navToggle.toggleAttribute("aria-expanded", false)
+  primaryNav.hasAttribute("data-visible")
+    ? navToggle.toggleAttribute("aria-expanded", false)
       : navToggle.toggleAttribute("aria-expanded", true);
-    primaryNav.toggleAttribute("data-visible")
+  primaryNav.toggleAttribute("data-visible");
+  primaryHeader.toggleAttribute("data-overlay");
 });
